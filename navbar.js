ff5986f942174b3d2fb749aec103c30e6c15cdc9
kamil <kamil.wec@gmail.com>
2020-03-14 18:02:19 +0100

    add navbar component

---
+++ navbar.js
@@ -0,0 +1,3 @@
+let nav = '<nav><a href="index.html">Home</nav>' + '<nav><a href="contact.html">Kontakt</nav>' + '<nav><a href="quiz.html">Quiz</nav>';
+let navbarElem = document.querySelector(".navbar");
+navbarElem.innerHTML = nav;
